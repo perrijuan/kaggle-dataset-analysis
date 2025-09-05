cc4f54cbff44780f7bdb522268a4bd5ec106d28d
Juan perri <111884460+perrijuan@users.noreply.github.com>
2025-09-05 16:02:23 -0300

    Remove Firebase config from firebase.ts
    Removed sensitive Firebase configuration details.

--- hackthona mob 4/ufrjhackathon/hackathona/src/config/firebase.ts
+++ hackthona mob 4/ufrjhackathon/hackathona/src/config/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbPHEvXs2IRJz3wz64Sz1ugZWD0Dyt7yc",
-  authDomain: "hackatonamob4.firebaseapp.com",
-  projectId: "hackatonamob4",
-  storageBucket: "hackatonamob4.firebasestorage.app",
-  messagingSenderId: "507969476135",
-  appId: "1:507969476135:web:1a25499c72d8708e4a2002",
-  measurementId: "G-X485Y54BL9",
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 };
 
 // Initialize Firebase
